bffd5cb523d47eb69b8166f6101283e619a8cb0b
Ji Young Lee <jylee1103@gmail.com>
2015-02-11 11:04:41 +1100

    Implemented retrieval of access token from twitter oauth

--- server/api/twitter/twitter.controller.js
+++ server/api/twitter/twitter.controller.js
@@ -9,13 +9,43 @@
 
 'use strict';
 
-var _ = require('lodash');
+var https = require('https');
 
-// Get list of things
 exports.get = function (req, res) {
-  res.json(
-    {
-      test: 'twitter/token',
-      access_token: 'dummy_access_token'
+
+  var consumerKey = encodeURIComponent(process.env.TWITTER_KEY);
+  var consumerSecret = encodeURIComponent(process.env.TWITTER_SECRET);
+  var credentials = new Buffer(consumerKey + ':' + consumerSecret).toString('base64');
+
+  var options = {
+    host: 'api.twitter.com',
+    path: '/oauth2/token',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+      'Authorization': 'Basic ' + credentials
+    }
+  };
+
+  var reqPost = https.request(options, function (response) {
+
+    response.on('data', function (d) {
+      var tokenJSONObject = JSON.parse((new Buffer(d, 'base64')).toString());
+      var accessToken = tokenJSONObject.access_token;
+      var encodedAccessToken = new Buffer(accessToken).toString('base64');
+
+      console.info('Retrieved Access token : ' + encodedAccessToken);
+
+      res.json({access_token: encodedAccessToken});
     });
+  });
+  reqPost.write("grant_type=client_credentials");
+
+  reqPost.on('error', function (e) {
+    console.error(e);
+    res.status(401);
+  });
+
+  reqPost.end();
+
 };
