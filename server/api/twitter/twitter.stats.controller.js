2eca1fec74f4e13db5ae6cbc65990cd0d2fc7bb1
Ji Young Lee <jylee1103@gmail.com>
2015-02-12 21:05:54 +1100

    Fixed Error cases when number of tweets doesn't align with number of returned tweets, Error not propagating to client

--- server/api/twitter/twitter.stats.controller.js
+++ server/api/twitter/twitter.stats.controller.js
@@ -5,29 +5,33 @@ var aggregator = require('./twitter.hourly.stats.aggregator.js')
 
 exports.get = function (req, res) {
 
-  console.info('Started retriieving timeline ' + req.header('Authorization'));
   var tweets = [];
-
-  console.info('GetTwitts');
+  var max_id = "";
+  console.info('Number of Tweets ' + tweets.length);
 
   var callback = function (data) {
     tweets = tweets.concat(data);
+    console.info('Number of Tweets ' + tweets.length);
+
     var lastTweet = tweets[tweets.length - 1];
-    var num_of_tweets = lastTweet.statuses_count;
+    var num_of_tweets = lastTweet.user.statuses_count;
     var max_number_of_tweets = 500;
+
+    console.info('num_of_tweets ' + num_of_tweets);
     if (max_number_of_tweets > num_of_tweets) {
       max_number_of_tweets = num_of_tweets;
     }
 
-    console.info("________ Callback " + tweets.length + ' ' + req.param('twitterScreenName'));
+    console.info(max_number_of_tweets + "________ Callback " + tweets.length);
 
-    if (tweets.length < max_number_of_tweets) {
+    if (tweets.length < max_number_of_tweets && max_id != lastTweet.id_str) {
       console.info('GetTwitts' + tweets[tweets.length - 1]);
-      getTwitts(req, res, lastTweet.id_str, callback);
+      max_id = lastTweet.id_str;
+      getTweets(req, res, max_id, callback);
     }
     else {
-      if (tweets.length > max_number_of_tweets) {
-        console.info("length - 500 = " + tweets.length - 500)
+      if (tweets.length > 500) {
+        console.info("length - 500 = " + tweets.length - 500);
         tweets.splice(500, (tweets.length - 500));
       }
       console.info('spliced tweets' + tweets.length);
@@ -35,11 +39,13 @@ exports.get = function (req, res) {
     }
   };
 
-  getTwitts(req, res, "", callback);
+  getTweets(req, res, max_id, callback);
 
 };
 
-function getTwitts(req, res, max_id, callback) {
+function getTweets(req, res, max_id, callback) {
+
+  console.info('Get Tweets with max_id ' + max_id);
 
   function buildOptions(req, max_id) {
     var options = {
@@ -50,7 +56,7 @@ function getTwitts(req, res, max_id, callback) {
       }
     };
 
-    console.info(req.header('twitterScreenName'));
+    console.info("max_id" + max_id);
     var path = '/1.1/statuses/user_timeline.json?' +
       '&count=200' +
       '&screen_name=' + req.param('twitterScreenName');
@@ -70,20 +76,30 @@ function getTwitts(req, res, max_id, callback) {
     var concatenatedData = "";
 
     response.on('data', function (d) {
-      console.info('Retrieved timeline : ' + d);
+      console.info('on Data : ');
       concatenatedData = concatenatedData.concat(d);
     });
 
     response.on('end', function () {
-      console.info('Retrieved timeline : ' + concatenatedData);
       var parsedData = JSON.parse(concatenatedData);
-      if (parsedData.errors) {
-        res.status(401, parsedData.errors.message);
+      console.info('On End : number of received data' + concatenatedData);
+      console.info(JSON.stringify(parsedData.errors));
+      console.info(parsedData.errors)
+      if (parsedData.errors != undefined) {
+        console.info('ERROR');
+        res.status(404);
+        res.send(parsedData.errors[0].message);
       }
       else {
+        console.info('First Item id ' + concatenatedData);
         if (max_id != "" && parsedData[0].id_str == max_id) {
           parsedData.splice(0, 1);
         }
+        if (max_id == "" && concatenatedData == "") {
+          res.json(aggregator.aggregate([]));
+        }
+
+        console.info('On End called back data: ');// + JSON.stringify(parsedData));
         callback(parsedData);
       }
     });
