b48fe974698e9d7019833bb02af973b9450d5c7a
Ji Young Lee <jylee1103@gmail.com>
2015-02-13 06:33:28 +1100

    Installed and Implemented histogram in main controller

--- client/app/app.js
+++ client/app/app.js
@@ -5,7 +5,8 @@ angular.module('twitterHourlyStatsApp', [
   'ngResource',
   'ngSanitize',
   'ngRoute',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'highcharts-ng'
 ])
   .config(function ($routeProvider, $locationProvider) {
     $routeProvider
--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,6 +4,11 @@ angular.module('twitterHourlyStatsApp')
   .controller('MainCtrl', function ($scope, $http) {
     $scope.hasToken = false;
     $scope.processing = false;
+    $scope.stats;
+
+    $scope.canDisplayChart = function () {
+      return $scope.stats != undefined && !$scope.processing;
+    };
 
     $http.get('/api/twitter/token')
       .success(function (token) {
@@ -24,7 +29,56 @@ angular.module('twitterHourlyStatsApp')
       $http.get('/api/twitter/stats', {params: {twitterScreenName: $scope.twitterScreenName}})
         .success(function (stats) {
           console.log('success' + JSON.stringify(stats));
+          $scope.stats = stats;
           $scope.processing = false;
+          $scope.chartConfig = {
+            options: {
+              xAxis: {
+                categories: Object.keys(stats),
+                title: {
+                  enabled: true,
+                  text: 'Hours',
+                  style: {
+                    fontWeight: 'normal'
+                  }
+                }
+              },
+              yAxis: {
+                title: {
+                  enabled: true,
+                  text: 'Tweets',
+                  style: {
+                    fontWeight: 'normal'
+                  }
+                }
+              },
+              chart: {
+                type: 'column'
+              },
+              navigation: {
+                buttonOptions: {
+                  enabled: false
+                }
+              },
+              tooltip: {
+                enabled: false
+              }
+            },
+            series: [{
+              data: Object.keys(stats).map(function (key) {
+                return stats[key];
+              }),
+              showInLegend: false,
+              color: 'rgb(67, 147, 185)'
+            }],
+            credits: {
+              enabled: false
+            },
+            title: {
+              text: $scope.twitterScreenName
+            },
+            loading: false
+          };
         })
         .error(function (error) {
           $scope.processing = false;
